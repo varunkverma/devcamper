4dd5cc6a03d765d1eaff0ed673fa993b046b47b7
varunkverma <varunkverma1996@gmail.com>
2020-03-15 13:57:15 +0530

    implemented controller for bootcamps routes

--- routes/bootcamps.routes.js
+++ routes/bootcamps.routes.js
@@ -1,40 +1,23 @@
 const express = require("express");
-
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.status(200).json({
-    success: true,
-    msg: "Show all bootcamps"
-  });
-});
+const {
+  getBootcamps,
+  getBootcamp,
+  createBootcamp,
+  updateBootcamp,
+  deleteBootcamp
+} = require("../controllers/bootcamps.controller");
 
-router.get("/:id", (req, res) => {
-  res.status(200).json({
-    success: true,
-    msg: `Show bootcamp ${req.params.id}`
-  });
-});
+router
+  .route("/")
+  .get(getBootcamps)
+  .post(createBootcamp);
 
-router.post("/", (req, res) => {
-  res.status(201).json({
-    success: true,
-    msg: "Create a bootcamps"
-  });
-});
-
-router.put("/:id", (req, res) => {
-  res.status(200).json({
-    success: true,
-    msg: `update bootcamp ${req.params.id}`
-  });
-});
-
-router.delete("/:id", (req, res) => {
-  res.status(200).json({
-    success: true,
-    msg: `delete bootcamp ${req.params.id}`
-  });
-});
+router
+  .route("/:id")
+  .get(getBootcamp)
+  .put(updateBootcamp)
+  .delete(deleteBootcamp);
 
 module.exports = router;
